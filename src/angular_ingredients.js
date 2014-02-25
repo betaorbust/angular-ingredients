c635abfdb2136eed898344362179792412d4d35e
Jacques Favreau <jacques@udacity.com>
2014-02-25 04:53:15 -0800

    Add(Site): Add base module for ingredients.

---
+++ src/angular_ingredients.js
@@ -0,0 +1,6 @@
+// =======================
+// = Module Declarations =
+// =======================
+
+// Initializes the main  module, which all the others rely on.
+angular.module('ingredients', []);
