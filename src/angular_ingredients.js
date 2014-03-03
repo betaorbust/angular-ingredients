28afe976774429d2984165377719ab014f519ff8
Jacques Favreau <jacques@udacity.com>
2014-03-03 07:48:40 -0800

    Docs (Main): Better document the main module.

--- src/angular_ingredients.js
+++ src/angular_ingredients.js
@@ -1,6 +1,16 @@
-// =======================
-// = Module Declarations =
-// =======================
+/**
+ * @ngdoc overview
+ * @name ingredients
+ * @description
+ *
+ * # ingredients
+ *
+ * The `ingredients` module provides a collection of usefule services for accomplishing common
+ * tasks that people run into when building projects with Angular.
+ *
+ * Each of the individual components of `ingredients` can be used separately and each comes with
+ * its own documentation and tests.
+ */
 
 // Initializes the main  module, which all the others rely on.
 angular.module('ingredients', []);
